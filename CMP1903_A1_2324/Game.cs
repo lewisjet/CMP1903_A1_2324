5976c3849644fd083182d844fddcc37c319e21af
Lewis Page <lewisjet11@outlook.com>
2024-02-22 02:42:28 +0000

    Create core game implementation
    I created the Die class, capable of rolling itself and storing its result in an encapsulated property.
    
    The Game class holds 3 dice, able to roll them all simultaneously, summate them, output their results and continuously roll dice.
    
    The Program static class holds the main method for the program, instantiating the game class and making it roll dice continuously. The main method also instantiates the testing class for the program, running it after the game is completed. The class also holds an instance of the Random class as a singleton, to prevent over-instantiation errors.
    
    The Testing class holds various tests for the game, ensuring that both the Game class and the Die class work as intended.

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
@@ -12,10 +10,105 @@ namespace CMP1903_A1_2324
          * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
          *
          * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
+         * rolls could be continuous, and the totals and other statistics could be summarised for example.
          */
 
-        //Methods
+        // A variable, holding an array of three dice, which are used in the game.
+        // Since the number of dice is constant, the array can be made readonly.
+        private readonly Die[] _dice = {
+            new Die(),
+            new Die(),
+            new Die()
+        };
 
+        /// <summary>
+        /// Rolls all the dice in the Game class.
+        /// </summary>
+        /// <returns>The results of all of the dice, rolled.</returns>
+        public IEnumerable<int> RollAllDice() => 
+            // Roll each of the dice in the class, concatenating their results into an enumerable.
+            _dice.Select(dice => dice.Roll());
+
+        /// <summary>
+        /// Returns the total of all the current die values.
+        /// </summary>
+        /// <returns>The total sum of all current die values.</returns>
+        public int SumAllDice() => _dice.Sum(die => die.CurrentValue);
+
+        /// <summary>
+        /// Prints dice roll results to the console.
+        /// </summary>
+        /// <param name="diceRolls">The current values of each dice being displayed.</param>
+        private void ReportDiceRolls(IEnumerable<int> diceRolls)
+        {
+            /*
+             * The parameter for ReportDiceRolls is the generic type IEnumerable.
+             * To work with the data, it needs converting to an array.
+             */
+            var diceRollArray = diceRolls.ToArray();
+            
+            // An integer is defined to hold the sum of dice rolls in.
+            // Note: the SumAllDice method is not used for interoperability reasons.
+            var diceRollSum = 0;
+            
+            // Iterate through every dice roll in the array
+            for (var diceIndex = 0; diceIndex < diceRollArray.Length; diceIndex++)
+            {
+                // Get the dice roll from the array
+                var roll = diceRollArray[diceIndex];
+                
+                // Add the dice roll to the total sum
+                diceRollSum += roll;
+                
+                // Print the dice roll to the console
+                Console.WriteLine($"Dice roll {diceIndex + 1}: {roll}");
+            }
+            
+            // Print a blank line to the console
+            Console.WriteLine();
+            
+            // Print the sum of all rolls to the console
+            Console.WriteLine($"Sum of dice rolls: {diceRollSum}");
+        }
+
+        /// <summary>
+        /// Rolls all dice in the Game class and then outputs their new values to the console.
+        /// </summary>
+        public void RollDiceAndPrintValues()
+        {
+            // Roll all dice in the Game class, getting their new values
+            var diceRolls = RollAllDice();
+            
+            // Output these new values to the console
+            ReportDiceRolls(diceRolls);
+        }
+
+        /// <summary>
+        /// Rolls all dice and prints their values until the user inputs anything but 'y'
+        /// </summary>
+        public void RollDiceContinuously()
+        {
+            // Until further notice,
+            while (true)
+            {
+                // Roll all Dice in the game class and print their new values.
+                RollDiceAndPrintValues();
+                
+                // Print a blank line, followed by a prompt on what to do next.
+                Console.WriteLine();
+                Console.WriteLine("Would you like to roll again?");
+                Console.WriteLine("Press 'y' to roll again, or anything else to quit.");
+
+                // Get the keypress from the end-user
+                var keyPress = Console.ReadKey();
+                
+                // If the user inputs 'Y' (in case of Caps-Lock) or 'y', break out of the function.
+                if(keyPress.KeyChar.ToString().ToLower() != "y") return;
+                
+                // Else, clear the console and repeat the loop.
+                Console.Clear();
+            }
+        }
+        
     }
 }
--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,69 @@ namespace CMP1903_A1_2324
          * Use debug.assert() to make the comparisons and tests.
          */
 
-        //Method
+        // Since testing is done on the game, a readonly variable to store the Game class in has been made.
+        private readonly Game _gameTested;
+
+        /// <summary>
+        /// The constructor for the Testing class
+        /// </summary>
+        /// <param name="gameTested">The game to test</param>
+        public Testing(Game gameTested)
+        {
+            // Sets the _gameTested variable to the game which is being tested by the class.
+            _gameTested = gameTested;
+        }
+
+        /// <summary>
+        /// Validates that both the RollAllDice and SumAllDice functions work as intended.
+        /// This is done by finding the sum of the dice rolls given, and the sum of the dice rolls stored
+        /// and comparing the two in an assert function.
+        /// </summary>
+        private void ValidateSumOfDice()
+        {
+            // Run the roll all dice function, to get an enumerable of each die result.
+            var diceRolls = _gameTested.RollAllDice();
+            
+            // Find the sum of all the dice rolls, gotten from the RollAllDice function.
+            var sumOne = diceRolls.Sum();
+            
+            // Get the sum of all dice rolls from the SumAllDice function.
+            var sumTwo = _gameTested.SumAllDice();
+
+            // Asset that both of the sums are the same.
+            Debug.Assert(sumOne == sumTwo);
+        }
+
+        /// <summary>
+        /// Creates a die instance, rolls it, and checks that its result is the same as the die's current value.
+        /// The method also checks that the value is between 1 and 6.
+        /// This method has been made static since it does not rely on the game class.
+        /// </summary>
+        private static void CheckDiceRollAsIntended()
+        {
+            // Instantiate a new Die instance.
+            var die = new Die();
+            
+            // Roll the new die,storing the result in a variable.
+            var result = die.Roll();
+            
+            // Assert that the result equals the current value of the die.
+            Debug.Assert(result == die.CurrentValue);
+            
+            // Check that the value is between 1 and 6.
+            Debug.Assert(result > 0 && result < 7);
+        }
+
+        /// <summary>
+        /// Runs each of the testing functions in the class.
+        /// </summary>
+        public void RunTestingSuite()
+        {
+            // Checks that the dice work as intended.
+            CheckDiceRollAsIntended();
+            
+            // Checks that the Game class can roll and sum correctly.
+            ValidateSumOfDice();
+        }
     }
 }
